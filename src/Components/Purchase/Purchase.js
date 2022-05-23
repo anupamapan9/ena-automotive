53f8e15ce23ef2ae6fb10ef7a39708058130b8ec
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-23 12:09:47 +0600

    added order to db

--- src/Components/Purchase/Purchase.js
+++ src/Components/Purchase/Purchase.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 import useSingleProduct from '../../Hooks/useSingleProduct';
@@ -7,8 +8,7 @@ import Loading from '../Common/Loading';
 
 const Purchase = () => {
     const { id } = useParams();
-    // const [product, setProduct] = useState({});
-    // use
+
     const [product, isLoading, refetch] = useSingleProduct(id)
     const [user, loading] = useAuthState(auth)
     if (isLoading || loading) {
@@ -16,6 +16,42 @@ const Purchase = () => {
     }
     const { name, image, description, min_order, price, quantity } = product;
 
+
+    const handelPlaceOrder = e => {
+        e.preventDefault()
+        const ordered_quantity = parseInt(e.target.ordered_quantity.value);
+        const address = e.target.address.value;
+        if (min_order > ordered_quantity) {
+            toast.error(`Opps!! Please Order more than ${min_order}`)
+        } else if (ordered_quantity > quantity) {
+            toast.error(`Sorry we haven't stock more than ${quantity}`)
+        } else {
+            const total_price = (ordered_quantity * price).toFixed(2);
+            const orderedProduct = {
+                orderer: user?.email,
+                ordered_product: name,
+                image,
+                ordered_quantity,
+                price,
+                total_price,
+                address,
+                status: 'unpaid'
+            }
+            fetch('http://localhost:5000/order', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(orderedProduct),
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+        }
+        e.target.reset()
+    }
 
     return (
         <div className='lg:px-20 flex justify-around items-center gap-10'>
@@ -37,7 +73,7 @@ const Purchase = () => {
                 </div>
             </div>
             <div className='w-1/2'>
-                <form>
+                <form onSubmit={handelPlaceOrder}>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
@@ -52,12 +88,25 @@ const Purchase = () => {
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text">Your Quantity( <span className='text-warning'>should more the {min_order} and less than {quantity}</span> )</span>
                         </label>
-                        <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                        <input type="number" name='ordered_quantity' placeholder="Please Enter your quantity" className="input input-bordered" />
                     </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Phone Number</span>
+                        </label>
+                        <input type="number" name='phone' required placeholder="Please Enter your Phone Number" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Shipping address</span>
+                        </label>
+                        <input type="text" name='address' required placeholder="Enter shipping address" className="input input-bordered" />
+                    </div>
+
                     <div className="form-control mt-6">
-                        <input type="submit" value='Sign Up' className="btn btn-primary" />
+                        <input type="submit" value='Place Order' className="btn btn-primary" />
                     </div>
                 </form>
             </div>
