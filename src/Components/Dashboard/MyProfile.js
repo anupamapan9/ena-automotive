c51f379b70ba3657dcbb8869254187332025e753
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-25 00:27:31 +0600

    updated user functionality added

--- src/Components/Dashboard/MyProfile.js
+++ src/Components/Dashboard/MyProfile.js
@@ -1,9 +1,150 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import toast from 'react-hot-toast';
+import { BsPencilFill } from 'react-icons/bs'
+import auth from '../../firebase.init';
+import useUserprofile from '../../Hooks/useUserprofile';
+import Loading from '../Common/Loading';
 const MyProfile = () => {
+    const [isEdit, setEdit] = useState(false)
+    const manageEdit = () => {
+        setEdit(!isEdit)
+    }
+    const [user, loading] = useAuthState(auth)
+    const email = user?.email;
+    console.log(email)
+    const [updatedUser, isLoading, refetch] = useUserprofile(email)
+
+
+    console.log(updatedUser)
+    const handelEditUser = e => {
+        e.preventDefault()
+        const displayName = e.target.displayName.value;
+        const phone = e.target.phone.value;
+        const city = e.target.city.value;
+        const social = e.target.social.value;
+        const imageStorageKey = 'f721c5682ba4821a0a2817c153c428de';
+        const image = e.target.image.files[0];
+
+
+        const formData = new FormData();
+        formData.append('image', image);
+        // const url = `https://api.imgbb.com/1/upload?key=${imageKey}`
+
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.success) {
+                    const avatar = result.data.url
+                    const currentUser = {
+                        displayName,
+                        phone,
+                        city,
+                        social,
+                        avatar
+                    }
+                    fetch(`http://localhost:5000/user/update/${email}`, {
+                        method: 'PUT',
+                        headers: {
+                            'content-type': 'application/json',
+                            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(currentUser)
+                    }).then(res => res.json())
+                        .then(data => {
+                            if (data.modifiedCount > 0) {
+                                toast.success('successFully uploaded')
+                                e.target.reset()
+                                refetch()
+                            }
+                        })
+                }
+            })
+
+    }
+    if (isLoading || loading) {
+        return <Loading />
+    }
     return (
         <div>
-            <h2 className='text-xl font-bold inline-block'>My Profile</h2>
+            <h2 className='text-xl font-bold inline-block py-5'>My Profile</h2>
+            <div className='flex flex-col md:flex-row'>
+                <div className='md:w-5/12 px-10'>
+                    <div className='flex justify-between items-start bg-cyan-600 rounded-t' >
+                        <div class="avatar">
+                            <div class="w-24 rounded">
+                                <img src={updatedUser.avatar} alt='' />
+                            </div>
+                        </div>
+                        <button onClick={manageEdit} className='bg-primary p-2 rounded text-white'><p><BsPencilFill className='inline-block' />  Edit</p></button>
+                    </div>
+                    <div class="overflow-x-auto">
+                        <table class="table w-full">
+                            <tbody>
+                                {/* - row 1  */}
+                                <tr>
+                                    <th className='hover:bg-cyan-500 bg-cyan-600 duration-300'>Name: {updatedUser.displayName}</th>
+                                </tr>
+                                {/* row 2  */}
+                                <tr >
+                                    <th className='hover:bg-cyan-600 bg-cyan-500 duration-300'>Email: {email}</th>
+                                </tr>
+                                {/* row3 */}
+                                <tr>
+                                    <th className='hover:bg-cyan-500 bg-cyan-600 duration-300'>Phone: {updatedUser.phone}</th>
+                                </tr>
+                                <tr>
+                                    <th className='hover:bg-cyan-600 bg-cyan-500 duration-300'>Address: {updatedUser.city}</th>
+                                </tr>
+                                <tr>
+                                    <th className='hover:bg-cyan-500 bg-cyan-600 duration-300'><a href={updatedUser.social} target="_blank" rel="noopener noreferrer">Linked In:</a></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+                {
+                    isEdit &&
+                    <div class="card  bg-cyan-500 text-neutral-content">
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title text-white">Update Your Profile</h2>
+                            <form onSubmit={handelEditUser}>
+                                <div class="form-control w-full max-w-xs">
+                                    <input type="text" placeholder="Enter Your Name" name='displayName' class="input input-bordered w-full max-w-xs" />
+                                </div>
+
+                                <div class="form-control w-full mt-2 max-w-xs">
+                                    <input type="text" placeholder="Enter Phone Number" class="input input-bordered w-full max-w-xs" name='phone' />
+                                </div>
+                                <div class="form-control w-full mt-2 max-w-xs">
+                                    <input type="text" placeholder="Enter City Name" name='city' class="input input-bordered w-full max-w-xs" />
+                                </div>
+                                <div class="form-control w-full mt-2 max-w-xs">
+                                    <input type="text" placeholder="Enter LinkedIn Profile name"
+                                        name='social'
+                                        class="input input-bordered w-full max-w-xs" />
+                                </div>
+                                <div class="form-control w-full mt-2 max-w-xs">
+                                    <input type="file" name='image' placeholder="Enter LinkedIn Profile name" class="w-full max-w-xs" />
+                                </div>
+                                <div class="card-actions">
+                                    <input type="submit" value="Update" class="btn btn-primary" />
+                                    <button class="btn btn-warning" onClick={() => setEdit(false)}>Cancel</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>}
+                <div>
+
+                </div>
+            </div>
+
 
         </div>
     );
---
+++ src/Hooks/useUserprofile.js
@@ -0,0 +1,16 @@
+import { useQuery } from "react-query";
+
+
+const useUserprofile = (email) => {
+    console.log(email)
+    const { data: updatedUser, isLoading, refetch } = useQuery(['updatedUser', email], () => fetch(`http://localhost:5000/user/${email}`, {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+        .then(res => res.json()))
+    return [updatedUser, isLoading, refetch]
+};
+
+export default useUserprofile;
